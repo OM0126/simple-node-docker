fd16ae0fffef10624b431cf19463d5abc911ddd1
om yengantiwar <omgithub01@gmail.com>
2026-07-24 21:29:53 +0530

    Clean up in app.js
    Removed commented lines regarding PUT HTTP verb and XSS mitigation.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express'),
     bodyParser = require('body-parser'),
-    // In order to use PUT HTTP verb to edit item
+    
     methodOverride = require('method-override'),
-    // Mitigate XSS using sanitizer
+   
     sanitizer = require('sanitizer'),
     app = express(),
     port = 8000
@@ -10,7 +10,7 @@ const express = require('express'),
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-// https: //github.com/expressjs/method-override#custom-logic
+
 app.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
         // look in urlencoded POST bodies and delete it
